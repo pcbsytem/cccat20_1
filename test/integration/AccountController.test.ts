02b4ec9bf674bcc7b7cc88a2d3b4fa8a7f2dac47
pcbsytem <pcbsytem@hotmail.com>
2025-04-11 10:56:27 -0300

    test: update input fields

--- test/integration/AccountController.test.ts
+++ test/integration/AccountController.test.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 import { faker } from '@faker-js/faker';
 
-axios.defaults.validateStatus = () => true
+axios.defaults.validateStatus = () => true;
 
 test('Deve fazer a criação da conta de um usuário do tipo passageiro', async () => {
   const input = {
@@ -9,18 +9,25 @@ test('Deve fazer a criação da conta de um usuário do tipo passageiro', async 
     email: faker.internet.email(),
     cpf: '97456321558',
     password: 'asdQWE123',
+    carPlate: '',
     isPassenger: true,
-  }
-  const responseSignup = await axios.post('http://localhost:3000/signup', input)
-  const outputSignup = responseSignup.data
-  expect(outputSignup.accountId).toBeDefined()
-  const responseGetAccount = await axios.get(`http://localhost:3000/accounts/${outputSignup.accountId}`)
-  const outputGetAccount = responseGetAccount.data
-  expect(outputGetAccount.name).toBe(input.name)
-  expect(outputGetAccount.email).toBe(input.email)
-  expect(outputGetAccount.cpf).toBe(input.cpf)
-  expect(outputGetAccount.password).toBe(input.password)
-  expect(outputGetAccount.isPassenger).toBe(input.isPassenger)
+    isDriver: false
+  };
+  const responseSignup = await axios.post(
+    'http://localhost:3000/signup',
+    input
+  );
+  const outputSignup = responseSignup.data;
+  expect(outputSignup.accountId).toBeDefined();
+  const responseGetAccount = await axios.get(
+    `http://localhost:3000/accounts/${outputSignup.accountId}`
+  );
+  const outputGetAccount = responseGetAccount.data;
+  expect(outputGetAccount.name).toBe(input.name);
+  expect(outputGetAccount.email).toBe(input.email);
+  expect(outputGetAccount.cpf).toBe(input.cpf);
+  expect(outputGetAccount.password).toBe(input.password);
+  expect(outputGetAccount.isPassenger).toBe(input.isPassenger);
 });
 
 test('Não deve fazer a criação da conta de uma usuário se o nome for inválido', async () => {
@@ -29,10 +36,15 @@ test('Não deve fazer a criação da conta de uma usuário se o nome for inváli
     email: faker.internet.email(),
     cpf: '97456321558',
     password: 'asdQWE123',
+    carPlate: '',
     isPassenger: true,
-  }
-  const responseSignup = await axios.post('http://localhost:3000/signup', input)
-  expect(responseSignup.status).toBe(422)
-  const outputSignup = responseSignup.data
-  expect(outputSignup.message).toBe("Invalid name")
+    isDriver: false
+  };
+  const responseSignup = await axios.post(
+    'http://localhost:3000/signup',
+    input
+  );
+  expect(responseSignup.status).toBe(422);
+  const outputSignup = responseSignup.data;
+  expect(outputSignup.message).toBe('Invalid name');
 });
