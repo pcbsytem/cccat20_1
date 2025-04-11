9d6dd37b601ecacb8fc32fe2cf204e1d6e674e29
pcbsytem <pcbsytem@hotmail.com>
2025-04-11 10:55:58 -0300

    test: update unit test

--- test/integration/ORM.test.ts
+++ test/integration/ORM.test.ts
@@ -1,17 +1,25 @@
 import { UUID } from '../../src/domain/vo/UUID';
-import { PgPromiseAdapter } from '../../src/infra/database/DatabaseConnection'
+import { PgPromiseAdapter } from '../../src/infra/database/DatabaseConnection';
 import Registry from '../../src/infra/di/Registry';
 import ORM from '../../src/infra/ORM/ORM';
 import { AccountModel } from '../../src/infra/ORM/ORM';
 
-test("Deve persistir usando um ORM", async () => {
+test('Deve persistir usando um ORM', async () => {
   const connection = new PgPromiseAdapter();
-  Registry.getInstance().provide("connection", connection);
+  Registry.getInstance().provide('databaseConnection', connection);
   const orm = new ORM();
-  const accountModel = new AccountModel(UUID.create().getValue(), "John Doe", "john.doe@gmail.com", "111.111.111-11");
+  const accountId = UUID.create().getValue();
+  const accountModel = new AccountModel(
+    accountId,
+    'John Doe',
+    'john.doe@gmail.com',
+    '111.111.111-11',
+    '111111',
+    'AAA9999',
+    true,
+    true
+  );
   await orm.save(accountModel);
-  // const persistedAccountModel = await orm.get(accountModel, "account_id", "uuid");
-  // expect(persistedAccountModel.name).toBe("John Doe");
-  // expect(persistedAccountModel.email).toBe("john.doe@gmail.com");
-  // expect(persistedAccountModel.cpf).toBe("111.111.111-11");
-})
+  await orm.get(AccountModel, 'account_id', accountId);
+  await connection.close();
+});
