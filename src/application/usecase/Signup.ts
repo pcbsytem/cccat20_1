8f7e4b89b2c3c7cc03129d6e2bd923d227589821
pcbsytem <pcbsytem@hotmail.com>
2025-04-11 10:47:43 -0300

    refactor: update code style

--- src/application/usecase/Signup.ts
+++ src/application/usecase/Signup.ts
@@ -1,30 +1,32 @@
 import { Account } from '../../domain/Account';
-import AccountRepository from "../../infra/repository/AccountRepository";
+import AccountRepository from '../../infra/repository/AccountRepository';
 import { inject } from '../../infra/di/Registry';
 
 export default class Signup {
-	@inject("accountRepository")
-	accountRepository!: AccountRepository
+  @inject('accountRepository')
+  accountRepository!: AccountRepository;
 
-	async execute(input: any): Promise<Output> {
-		const account = Account.create(
-			input.name, 
-			input.email, 
-			input.cpf, 
-			input.password, 
-			input.carPlate, 
-			input.isPassenger,
-			input.isDriver
-		);
-		const existingAccount = await this.accountRepository.getAccountByEmail(account.getEmail());
-		if (existingAccount) throw new Error("Account already exists");		
-		await this.accountRepository.saveAccount(account);
-		return {
-			accountId: account.getAccountId()
-		}
-	}
+  async execute(input: any): Promise<Output> {
+    const account = Account.create(
+      input.name,
+      input.email,
+      input.cpf,
+      input.password,
+      input.carPlate,
+      input.isPassenger,
+      input.isDriver
+    );
+    const existingAccount = await this.accountRepository.getAccountByEmail(
+      account.getEmail()
+    );
+    if (existingAccount) throw new Error('Account already exists');
+    await this.accountRepository.saveAccount(account);
+    return {
+      accountId: account.getAccountId()
+    };
+  }
 }
 
 type Output = {
-  accountId: string
-}
+  accountId: string;
+};
